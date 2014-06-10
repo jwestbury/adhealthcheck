84a55bd96d0d7039cc0bc800194a06777dbca956
jwestbury <jameswestbury@gmail.com>
2014-06-10 11:12:25 -0700

    added dfsr check

---
+++ check_dfsr_log.ps1
@@ -0,0 +1,13 @@
+﻿if((Get-EventLog -LogName 'DFS Replication')[0].EventID -eq 2213) {
+    write-host "DFS Replication failed and requires manual intervention. See event log."
+    exit 2
+} elseif((Get-EventLog -LogName 'DFS Replication')[0].EntryType -eq "Error") {
+    write-host "DFS Replication errors present. See event log."
+    exit 2
+} elseif((Get-EventLog -LogName 'DFS Replication')[0].EntryType -ne "Information") {
+    write-host "DFS Replication warnings present. See event log."
+    exit 1
+} else {
+    write-host "DFS Replication is okay."
+    exit 0
+}
